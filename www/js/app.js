8ab4b2d0cc2824dd9368a5f723969d7bcd0331ac
Philip Cesar Garay <philipgaray2@gmail.com>
2016-05-29 11:53:44 +0800

    demonstrate socket

--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,15 @@
 'use strict';
 
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
-    .run(function($ionicPlatform) {
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services',
+        'btford.socket-io',
+        'ngCordova'
+    ])
+    .constant('API_URL', 'http://localhost:3001')
+    // .constant('API_URL', 'http://wifi-self-healing.herokuapp.com/') // PRODUCTION
+    .constant('API_VERSION', '/api/1.0/')
+    .run(function($rootScope, $ionicPlatform, API_URL, $cordovaNetwork, $cordovaDevice, $cordovaGeolocation) {
+        $rootScope.item = {};
+
         $ionicPlatform.ready(function() {
             if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
                 cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -11,9 +19,77 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
             if (window.StatusBar) {
                 StatusBar.styleDefault();
             }
+
+            if (!window.socketIo) {
+                window.socketIo = window.io.connect(API_URL + '/?token=null');
+            } else {
+                window.io.disconnect();
+                window.socketIo = window.io.connect(API_URL + '/?token=null');
+            }
+
+            $rootScope.online = $cordovaNetwork.isOnline();
+
+
+            var posOptions = { timeout: 10000, enableHighAccuracy: false };
+            $cordovaGeolocation
+                .getCurrentPosition(posOptions)
+                .then(function(position) {
+                    $rootScope.item.latitude = position.coords.latitude
+                    $rootScope.item.longitude = position.coords.longitude
+                }, function(err) {
+                    console.log('error $cordovaGeolocation: ', err);
+                });
+
+            window.MacAddress.getMacAddress(
+                function(macAddress) {
+                    $rootScope.item.macAddress = macAddress;
+                },
+                function(error) {
+                    console.log('device MacAddress err: ', error);
+                }
+            );
+
+            $rootScope.item.device = $cordovaDevice.getName();
+            $rootScope.item.model = $cordovaDevice.getModel();
+            $rootScope.item.platform = $cordovaDevice.getPlatform();
+            $rootScope.item.deviceId = $cordovaDevice.getUUID();
+            $rootScope.item.manufacturer = $cordovaDevice.getManufacturer();
+            $rootScope.item.version = $cordovaDevice.getVersion();
+
+            console.log('run $scope.item: ', $rootScope.item);
+            socketIo.emit('socket', $scope.item);
+
+            $rootScope.$on('$cordovaNetwork:online', function(event, networkState) {
+                var onlineState = networkState;
+
+                $rootScope.$apply(function() {
+                    $rootScope.online = true;
+                    $rootScope.item.online = true;
+                    socketIo.emit('networkState', $rootScope.item);
+                });
+
+            });
+
+            // listen for Offline event
+            $rootScope.$on('$cordovaNetwork:offline', function(event, networkState) {
+                var offlineState = networkState;
+
+                $rootScope.$apply(function() {
+                    $rootScope.online = false;
+                    $rootScope.item.online = false;
+                    socketIo.emit('networkState', $rootScope.item);
+                    socketIo.emit('networkState', $rootScope.item);
+                });
+            });
+
+
+            socketIo.on('server:response',function(response){
+                console.log('server:response after ping: ', response);
+                socketIo.emit('device:ping', $rootScope.item);
+            });
         });
     })
-    .config(function($stateProvider, $urlRouterProvider,$ionicConfigProvider) {
+    .config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
 
         $ionicConfigProvider.backButton.previousTitleText(true);
 
@@ -50,7 +126,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
             });
 
         // if none of the above states are matched, use this as the fallback
-        $urlRouterProvider.otherwise('/account');
+        $urlRouterProvider.otherwise('/dash');
 
     })
     .directive('errSrc', function() {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -42,94 +42,54 @@ angular.module('starter.controllers', [])
         });
 
     })
-    .controller('DashCtrl', function($scope) {})
-    /*.controller('ChatsCtrl', function($scope, Chats) {
-        var app = {
-            initialize: function() {
-                this.bindEvents();
-            },
-            bindEvents: function() {
-                document.getElementById('startCameraButton').addEventListener('mousedown', this.onStartCamera, false);
-                document.getElementById('startCameraAnotherPosButton').addEventListener('mousedown', this.onStartCameraAnotherPos, false);
+    .controller('DashCtrl', function($scope, $cordovaDevice, $cordovaGeolocation) {
+        console.log('DashCtrl');
 
-                document.getElementById('stopCameraButton').addEventListener('mousedown', this.onStopCamera, false);
-                document.getElementById('takePictureButton').addEventListener('mousedown', this.onTakePicture, false);
-                document.getElementById('switchCameraButton').addEventListener('mousedown', this.onSwitchCamera, false);
-                document.getElementById('showButton').addEventListener('mousedown', this.onShow, false);
-                document.getElementById('hideButton').addEventListener('mousedown', this.onHide, false);
-                document.getElementById('colorEffectCombo').addEventListener('change', this.onColorEffectChanged, false);
-                //window.addEventListener('orientationchange', this.onStopCamera, false);
-                document.addEventListener('deviceready', this.onDeviceReady, false);
-            },
-            onStartCamera: function() {
-                var tapEnabled = true;
-                var dragEnabled = true;
-                var toBack = true;
-                cordova.plugins.camerapreview.startCamera({
-                    x: 0,
-                    y: 50,
-                    width: 300,
-                    height: 300
-                }, "front", tapEnabled, dragEnabled, toBack);
-            },
-            onStartCameraAnotherPos: function() {
-                var tapEnabled = true;
-                var dragEnabled = true;
-                var toBack = false;
-                cordova.plugins.camerapreview.startCamera({
-                    x: 200,
-                    y: 0,
-                    width: 100,
-                    height: 150
-                }, "front", tapEnabled, dragEnabled, toBack);
-            },
-            onStopCamera: function() {
-                cordova.plugins.camerapreview.stopCamera();
-            },
-            onTakePicture: function() {
-                cordova.plugins.camerapreview.takePicture({
-                    maxWidth: 640,
-                    maxHeight: 640
-                });
-            },
-            onSwitchCamera: function() {
-                cordova.plugins.camerapreview.switchCamera();
-            },
-            onShow: function() {
-                cordova.plugins.camerapreview.show();
-            },
-            onHide: function() {
-                cordova.plugins.camerapreview.hide();
-            },
-            onColorEffectChanged: function() {
-                var effect = document.getElementById('colorEffectCombo').value;
-                cordova.plugins.camerapreview.setColorEffect(effect);
-            },
+        $scope.item = {};
 
-            // deviceready Event Handler
-            onDeviceReady: function() {
-                //on picture
-                cordova.plugins.camerapreview.setOnPictureTakenHandler(function(result) {
-                    document.getElementById('originalPicture').src = result[0]; //originalPicturePath;
-                    document.getElementById('previewPicture').src = result[1]; //previewPicturePath;
-                });
-            }
-        };
+        var posOptions = { timeout: 10000, enableHighAccuracy: false };
+        $cordovaGeolocation
+            .getCurrentPosition(posOptions)
+            .then(function(position) {
+                $scope.item.latitude = position.coords.latitude
+                $scope.item.longitude = position.coords.longitude
+            }, function(err) {
+                console.log('error $cordovaGeolocation: ', err);
+            });
 
+        if (window.MacAddress) {
+            window.MacAddress.getMacAddress(
+                function(macAddress) {
+                    $scope.item.macAddress = macAddress || '';
+                },
+                function(error) {
+                    console.log('device MacAddress err: ', error);
+                }
+            );
+        }
 
+        if (window.networkinterface) {
+            networkinterface.getIPAddress(function(ipAddress) {
+                $scope.item.ipAddress = ipAddress || '';
+            }, function(error) {
+                console.log('device ipAddress err: ', error);
+            });
+        }
 
         $scope.$on("$ionicView.enter", function(scopes, states) {
-            if (window.cordova && window.cordova.plugins) {
-                app.initialize();
-            }
-        });
+            $scope.item.device = $cordovaDevice.getName();
+            $scope.item.model = $cordovaDevice.getModel();
+            $scope.item.platform = $cordovaDevice.getPlatform();
+            $scope.item.deviceId = $cordovaDevice.getUUID();
+            $scope.item.manufacturer = $cordovaDevice.getManufacturer();
+            $scope.item.version = $cordovaDevice.getVersion();
 
-        $scope.$on("$ionicView.leave", function(scopes, states) {
-            if (window.cordova && window.cordova.plugins) {
-                cordova.plugins.camerapreview.stopCamera();
-            }
+            console.log('$ionicView.enter $scope.item: ', $scope.item);
+
+            socketIo.emit('socket', $scope.item);
+            socketIo.emit('device:ping', $scope.item);
         });
-    })*/
+    })
     .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
         $scope.chat = Chats.get($stateParams.chatId);
     })
